38cc77246c791e78bdb1b83a9d3c046535d72475
Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
2017-06-05 14:07:55 -0300

    Tratamento de entrada realizado. So entradas com inteiros positivos!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,37 +17,43 @@ public class Main {
      * @param args
      * @throws java.lang.Exception
      */
-    public static void main(String[] args) throws Exception{
-        
+    public static void main(String[] args) throws Exception {
+
         Integer a = 0, b = 0, c = 0;
         Scanner teclado = new Scanner(System.in);
         Triangulos triangulo = new Triangulos();
 //==================================================
-        while(a == 0){
-        System.out.println("Valor de A:");
-        try {
-            a = teclado.nextInt();
-        } catch (NumberFormatException ex) {
-            System.out.println("erro: " + ex + " digite apenas números!");
-        }
-        }
-//==================================================
-        while(b == 0){
-        System.out.println("Valor de B:");
-        try {
-            b = teclado.nextInt();
-        } catch (NumberFormatException ex) {
-            System.out.println("erro: " + ex + " digite apenas números!");
-        }
+        while (a <= 0) {
+            System.out.println("Valor de A:");
+            try {
+                a = teclado.nextInt();
+            } catch (Exception ex) {
+                System.out.println("erro: " + ex + " digite apenas números inteiros positivos!");
+                teclado.nextLine();
+                a = -1;
+            }
         }
 //==================================================
-        while(c == 0){
-        System.out.println("Valor de C:");
-        try {
-            c = teclado.nextInt();
-        } catch (NumberFormatException ex) {
-            System.out.println("erro: " + ex + " digite apenas números!");
+        while (b <= 0) {
+            System.out.println("Valor de B:");
+            try {
+                b = teclado.nextInt();
+            } catch (Exception ex) {
+                System.out.println("erro: " + ex + " digite apenas números inteiros positivos!");
+                teclado.nextLine();
+                b = -1;
+            }
         }
+//==================================================
+        while (c <= 0) {
+            System.out.println("Valor de C:");
+            try {
+                c = teclado.nextInt();
+            } catch (Exception ex) {
+                System.out.println("erro: " + ex + " digite apenas números Inteiros positivos!");
+                teclado.nextLine();
+                c = -1;
+            }
         }
 //==================================================
         System.out.print(triangulo.VerificarTipo(a, b, c));
